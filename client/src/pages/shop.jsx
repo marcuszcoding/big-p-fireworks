7ff31490ca6c2ed8f1d13264ad3edb910a331523
Domara Hawil <domara.hawil@hotmail.com>
2023-05-31 14:09:36 -0400

    react usestate and useeffect in shop

--- client/src/pages/shop.jsx
+++ client/src/pages/shop.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function Shop() {
+
+const [products, setProducts] = useState([])
+
+useEffect( () => {
+  axios.get('/api/products')
+  .then( (response) => {
+    setProducts(response.data.products)
+  })
+  .catch( () => {} )
+}, [])
+
   return (
-    <div>Shop</div>
+
+    <div>
+      {
+        products.map((product) => {
+          return (
+          <spam>
+            <img 
+            src={product.image_url}>
+            </img>
+            {product.product_name}
+            {product.price}
+            {/* {product.description} */}
+          </spam>
+          )
+        })
+      }
+    </div>
   )
+    
+  
 }
 
 export default Shop
