01c6fcfc56db05033a06fdc47928ac02201b847c
Domara Hawil <domara.hawil@hotmail.com>
2023-05-31 13:28:20 -0400

    fixed table in discounts to products

--- server/db/schemas/discounts.js
+++ server/db/schemas/discounts.js
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS discounts CASCADE;
 CREATE TABLE discounts (
     id SERIAL PRIMARY KEY,
     product_id INTEGER REFERENCES products(id),
-    category_id INTEGER REFERENCES product_categories(id),
+    category_id INTEGER REFERENCES products_categories(id),
     discount_percentage DECIMAL(5, 2),
     discount_type VARCHAR(55) NOT NULL,
     start_date DATE,
