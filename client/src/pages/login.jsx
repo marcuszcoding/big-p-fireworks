23038cfc69e1af3a7c3676ba2534e36a309252dc
marcuszcoding <marcuszcoding@gmail.com>
2023-06-01 14:40:34 -0600

    initial login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/Login.css';
 
-function Login() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    // You can send the email and password to the server or perform any other necessary operations
+    // For this example, let's just log the values
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
