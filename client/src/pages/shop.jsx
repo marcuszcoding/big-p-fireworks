ce91ac87e356043a778cd1c83170e0ce31bc0a2a
Domara Hawil <domara.hawil@hotmail.com>
2023-06-02 15:15:54 -0400

    added category for shop

--- client/src/pages/shop.jsx
+++ client/src/pages/shop.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 import "../styles/Shop.css"
+
 
 
 function Shop() {
 
 const [products, setProducts] = useState([]);
 const [categories, setCategories] = useState([])
+const [selectedCategory ,setSelectedCategory] = useState(null)
+
+const navigate = useNavigate()
+
+const handleClickProduct = (product_name) => {
+  navigate(`/shop/${product_name.toLowerCase().split(" ").join("-")}`)
+}
 
 useEffect( () => {
   axios.get('/api/products')
   .then( (response) => {
-    setProducts(response.data.products)
+    !selectedCategory
+    ?setProducts(response.data.products)
+    :setProducts(response.data.products.filter(p => p.category_id === selectedCategory))
   })
   .catch( () => {} )
-}, []);
+}, [selectedCategory]);
 
 useEffect( () => {
   axios.get('/api/products_category')
@@ -28,11 +39,16 @@ useEffect( () => {
   return (
     <section className="shop-section">
       <div className='sidebar-category'>
+      <span className="category-box" style={{backgroundColor: !selectedCategory && "tomato"}}>
+            <div onClick={() => setSelectedCategory(null)} className='category-name'>
+            All Products
+            </div>
+          </span>
       {
         categories.map((category) => {
           return (
-          <span className="category-box" key={category.id}>
-            <div className='category-name'>
+          <span className="category-box" key={category.id} style={{backgroundColor: selectedCategory === category.id && "tomato"}}>
+            <div onClick={() => setSelectedCategory(category.id)} className='category-name'>
             {category.category_name}
             </div>
           </span>
@@ -47,14 +63,16 @@ useEffect( () => {
            return (
            <span className="product-box" key={product.id}>
              <img className='image'
-              src={product.image_url}>
+              src={product.image_url}
+              onClick={() => handleClickProduct(product.product_name)}
+              >
              </img>
              <div className='product_name'>
               {product.product_name}
              </div>
               {product.price}
             <div>
-            <button>Add to Cart</button>
+            <button onClick={() => handleClickProduct(product.product_name)}>See Details</button>
             </div>
           </span>
           )
